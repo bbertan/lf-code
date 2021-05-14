1b3bbd60d2c5117dc4bf2dc6adbf92e75be81bd3
Brian Bertan <brian.bertan@1010data.com>
2021-05-14 08:40:32 -0400

    Error handling and updates to README

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# lightfeather
+# lightfeather coding challenge
+
+This project implements the LightFeather backend challenge API.
+
+## API
+
+### GET /api/supervisors
+
+Retrieve a list of supervisors from:
+
+    https://609aae2c0f5a13001721bb02.mockapi.io/lightfeather/
+
+and return as json containing a list of supervisor strings formatted as:
+
+    "{jurisdiction} - {lastName}, {firstName}"
+
+Sort alphabetcally by juristiction, lastName and firstName.
+Numeric jurisdictions are not included in the response.
+
+### POST /api/submit
+
+This endpoint accepts input json formatted as in this example:
+
+    {
+        "firstName" : "Dwight",
+        "lastName" : "Schrute",
+        "email" : "dwight.schrute@dundermifflin.com",
+        "phone" : "555-555-1234",
+        "supervisor" : "Michael Scott"
+    }
+
+("email" and "phone" are optional) and prints to the console as:
+
+    First Name: Dwight
+    Last Name: Schrute
+    Email: dwight.schrute@dundermifflin.com
+    Phone number: 555-555-1234
+    Supervisor: Michael Scott
+
+## Install, Build and Run
+
+The API runs in a Docker container.
+
+1) Retrieve the depository into an empty directory.
+
+2) Open a terminal and build the docker image:
+
+    docker build --tag lf-challenge .
+
+3) Start the container:
+
+    docker run --name lf-challenge -p 5000:5000 lf-challenge
+
+4) The endpoint urls are:
+
+http://127.0.0.1:5000/api/supervisors
+
+http://127.0.0.1:5000/api/submit
+
+The submit API can be tested with curl:
+
+    curl --header "Content-Type: application/json" \
+    --request POST \
+    --data '{"firstName" : "Dwight","lastName" : "Schrute", "email" : "dwight.schrute@dundermifflin.com", "phone" : "555-555-1234", "supervisor" : "Michael Scott" }' \
+    http://127.0.0.1:5000/api/submit
--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, json, jsonify
-app = Flask(__name__)
 import sys
 import os
 import requests
+from flask import Flask, request, json, jsonify
 
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS']=True
+
+
+# Test if string can be converted to integer.
 def is_integer(s):
     try: 
         int(s)
@@ -15,46 +19,87 @@ def is_integer(s):
 def challenge():
     return "LightFeather challenge"
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return "404 error"
 
+'''
+Retrieve a list of supervisors from:
+   https://609aae2c0f5a13001721bb02.mockapi.io/lightfeather/
+and return as json containing a list of supervisor strings formatted as:
+    "{jurisdiction} - {lastName}, {firstName}"
+Sort alphabetcally by juristiction, lastName and firstName
+Numeric jurisdictions should not be included in the response
+'''
 @app.route("/api/supervisors", methods=['GET'])
 def supervisors():
+    try:
+        # Print to show request
+        print("supervisors post")
 
-    # jfile = open('response.json',)
-    # jdata = json.load(jfile)
+        # Retrieve supervisor data as json.
+        r = requests.get(url = 'https://609aae2c0f5a13001721bb02.mockapi.io/lightfeather/managers')
+        jdata = r.json()
 
-    print("supervisors post")
+        # Create list of supervisors tuples while filtering integer juristictions
+        supervisors = [(super['jurisdiction'], super['lastName'], super['firstName']) 
+                    for super in jdata if not is_integer(super['jurisdiction'])]
+        
+        # Sort
+        sorted_supervisors = sorted(supervisors, key = lambda x: (x[0], x[2], x[1]))
+        
+        # Format
+        formatted = ["{} - {}, {}".format(*super) for super in sorted_supervisors]
+        
+        # Return list as json.
+        return jsonify(formatted)
 
-    r = requests.get(url = 'https://609aae2c0f5a13001721bb02.mockapi.io/lightfeather/managers')
-    jdata = r.json()
+    except Exception as ex:
+        print("Error in API supervisors:", file=sys.stderr)
+        print(ex, file=sys.stderr)
+        return jsonify({"api" : "supervisors", "status" : 500}), 500
 
-    supervisors = [(super['jurisdiction'], super['lastName'], super['firstName']) 
-                  for super in jdata if not is_integer(super['jurisdiction'])]
-    
-    sorted_supervisors = sorted(supervisors, key = lambda x: (x[0], x[2], x[1]))
-    formatted = ["{} - {}, {}".format(*super) for super in sorted_supervisors]
-    return(jsonify(formatted))
-
+'''
+Accepts input json formatted as in this example:
+    {
+        "firstName" : "Dwight",
+        "lastName" : "Schrute",
+        "email" : "dwight.schrute@dundermifflin.com",
+        "phone" : "555-555-1234",
+        "supervisor" : "Michael Scott"
+    }
+("email" and "phone" are optional) and prints to the console.
+'''
 @app.route("/api/submit", methods=['POST'])
 def submit():
 
     try:
         jdata = request.get_json()
+
         first = jdata["firstName"]
         print("First Name: {}".format(first), file=sys.stdout)
+
         last  = jdata["lastName"]
         print("Last Name: {}".format(last), file=sys.stdout)
+        
         if "email" in jdata:
             email = jdata["email"]
             print("Email: {}".format(email), file=sys.stdout)
+        
         if "phone" in jdata:
             phone = jdata["phone"]
             print("Phone number: {}".format(phone), file=sys.stdout)
+        
         supervisor  = jdata["supervisor"]
         print("Supervisor: {}".format(supervisor), file=sys.stdout)
-        return ""
+        
+        # Empty data string returned
+        return jsonify({"api" : "submit", "status" : 200})
 
     except Exception as ex:
+        print("Error in API submit:", file=sys.stderr)
         print(ex, file=sys.stderr)
+        return jsonify({"api" : "submit", "status" : 500}), 500
 
 if __name__ == "__main__":
     print (os.getcwd())
